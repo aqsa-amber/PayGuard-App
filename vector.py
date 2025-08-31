fb92d059b405e9645ba34aa6f4a117972a930710
aqsa-amber <amberaqsa2k3@gmail.com>
2025-08-31 16:25:06 +0530

    Delete vector.py

--- vector.py
+++
@@ -1,7 +0,0 @@
-from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
-
-def create_vector_store(texts, metadatas):
-    embeddings = OpenAIEmbeddings()
-    vector_store = FAISS.from_texts(texts, embeddings, metadatas=metadatas)
-    return vector_store
